7e66ef7ba82d5b0c329c2f94b38ccad6f1345439
Chrisleo-16 <152534621+Chrisleo-16@users.noreply.github.com>
2025-05-06 22:18:03 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,7 +76,7 @@ const Navbar = () => {
                 src="/images/-high-resolution-logo-grayscale-transparent.png"
                 alt="Logo"
                 style={{width:"200px"}}
-                className="mobile-name"/>
+                className="mobile-name logo"/>
             </Link>
             <div className="d-flex align-items-center">
               <div className="social-links" style={{ marginLeft: "-200px" }}>
